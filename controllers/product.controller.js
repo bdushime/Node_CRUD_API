396f1204fae95a11fa7d9b9d48fe0f53547cea74
bdushime <bdushime47@gmail.com>
2025-11-01 12:12:33 +0200

    adding controller function

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -27,6 +27,16 @@ const createProduct = async(req,res)=>{
     }catch(error){
         res.status(500).json({message: error.message})
     }
+};
+
+const updateProduct = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id,req.body);
+        if(!product){
+            return res.status(404).json({message:"Product"})
+        }
+    }
 }
 
 module.exports = {
