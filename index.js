aa2642a2482bf9641ce3bc063470e081d003ab44
bdushime <bdushime47@gmail.com>
2025-11-01 12:25:19 +0200

    adding controller function

--- index.js
+++ index.js
@@ -32,17 +32,6 @@ app.get('/api/products',async(req,res)=>{
     }
 })
 
-app.post('/api/products',async(req,res)=>{
-    try {
-       const product = await Product.create(req.body);
-       res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-
-
 
 mongoose.connect("mongodb+srv://adminBeni:12345@backenddb.pyy2dzv.mongodb.net/?appName=backendDB")
 .then(()=>{
