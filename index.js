b776f2f3ae170672658fb73d0985f92617421d09
bdushime <bdushime47@gmail.com>
2025-11-01 12:24:50 +0200

    adding controller function

--- index.js
+++ index.js
@@ -69,21 +69,6 @@ app.get('/api/product/:id',async (req,res)=>{
  })
 
 
- // delete a product
- app.delete('/api/product/:id',async (req,res)=>{
-    try{
-       const {id} = req.params;
-       const product = await Product.findByIdAndDelete(id);
-       if(!product){
-        return res.status(404).json({message: "Product not found"});
-       }
-
-       res.status(200).json({message: "Product deleted successfully"});
-    }catch(error){
-        res.status(500).json({message:error.message});
-    }
- })
-
 
 
 mongoose.connect("mongodb+srv://adminBeni:12345@backenddb.pyy2dzv.mongodb.net/?appName=backendDB")
