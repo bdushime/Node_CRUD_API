0de44732b2bd4b77faf5d92693be458cb0de87a5
bdushime <bdushime47@gmail.com>
2025-11-01 12:24:04 +0200

    adding controller function

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -46,14 +46,17 @@ const updateProduct = async(req,res)=>{
 
 const deleteProduct = async (req,res)=>{
     try{
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
+       const {id} = req.params;
+       const product = await Product.findByIdAndDelete(id);
+       if(!product){
+        return res.status(404).json({message: "Product not found"});
+       }
 
-        if(!product){
-            return res.status(404).json({message:""})
-        }
+       res.status(200).json({message: "Product deleted successfully"});
+    }catch(error){
+        res.status(500).json({message:error.message});
     }
-}
+ }
 
 module.exports = {
     getProducts,
