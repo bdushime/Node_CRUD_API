d1be5777cd81c2e09138c5528f2cd626e9f208d0
bdushime <bdushime47@gmail.com>
2025-11-04 16:34:24 +0200

    checking and fixing

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -11,7 +11,8 @@ const Product = require('../models/product.model');
 
 const getProducts = async(req,res)=>{
     try{
-        const products
+        const products = await Product.find({});
+        res.status(200).json(products);
     }
 }
 
