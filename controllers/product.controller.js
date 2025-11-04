a0c907020a0afec3917226b178f91c1326ddf246
bdushime <bdushime47@gmail.com>
2025-11-04 16:47:24 +0200

    checking and fixing

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -30,15 +30,15 @@ const getProducts = async (req,res)=>{
     }
   }
 
-  
-const createProduct = async(req,res)=>{
-    try{
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-};
+
+// const createProduct = async(req,res)=>{
+//     try{
+//         const product = await Product.create(req.body);
+//         res.status(200).json(product);
+//     }catch(error){
+//         res.status(500).json({message: error.message})
+//     }
+// };
 
 const updateProduct = async(req,res)=>{
     try{
