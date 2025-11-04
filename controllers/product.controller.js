0ac5ab6ca7937d8b8abddceba582c9e8ca0a3967
bdushime <bdushime47@gmail.com>
2025-11-04 16:33:29 +0200

    checking and fixing

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,14 +1,19 @@
 const Product = require('../models/product.model');
 
-const getProducts = async (req,res)=>{
+// const getProducts = async (req,res)=>{
+//     try{
+//         const products = await Product.find({});
+//         res.status(200).json(products);
+//     }catch(error){
+//         res.status(500).json({message: error.message})
+//     }
+// }
+
+const getProducts = async(req,res)=>{
     try{
-        const products = await Product.find({});
-        res.status(200).json(products);
-    }catch(error){
-        res.status(500).json({message: error.message})
+        const products
     }
 }
-
 
 // const getProduct = async (req,res)=>{
 //      try{
