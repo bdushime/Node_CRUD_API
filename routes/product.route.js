1d85708a2e740192e562f9f39a587774edb45d8a
bdushime <bdushime47@gmail.com>
2025-11-11 23:24:09 +0200

    checking and fixing

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,8 +3,7 @@ const Product = require("../models/product.model")
 const router = express.Router();
 const {getProducts, getProduct,createProduct,updateProduct,deleteProduct} = require('../controllers/product.controller');
 
-// routess
-
+// routes
 
 router.get('/',getProducts);
 
