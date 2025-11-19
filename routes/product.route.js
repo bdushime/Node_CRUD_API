f2589fd8b97054c8d460b19267cdb25a19ff492b
bdushime <bdushime47@gmail.com>
2025-11-19 23:30:01 +0200

    formating

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,7 +6,7 @@ const {getProducts, getProduct,createProduct,updateProduct,deleteProduct} = requ
 // routes
 router.get('/',getProducts);
 router.get("/:id",getProduct);
-router.get("/:id",getProduct);
+
 
 router.post("/",createProduct);
 
